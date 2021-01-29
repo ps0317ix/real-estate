cd076104d0af69544700eee784f2ee08ba7efbdc
ps0317ix <38696393+ps0317ix@users.noreply.github.com>
2021-01-30 08:08:03 +0900

    1/30 axios interceptor追加

--- src/main.js
+++ src/main.js
@@ -15,7 +15,33 @@ Vue.directive("border", function(el, binding){
   el.style.borderStyle = binding.arg;
 });
 
+// axiosを用いる際のbaseURL
 axios.defaults.baseURL = "https://firestore.googleapis.com/v1/projects/real-estate-4ff0d/databases/(default)/documents/";
+
+const interceptorsRequest = axios.interceptors.request.use(
+  config => {
+    // axiosでリクエストを行った際の処理
+    console.log('interceptors request', config);
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+const interceptorsResponse = axios.interceptors.response.use(
+  response => {
+    // axiosからレスポンスを受け取った際の処理
+    console.log('interceptors response', response);
+    return response;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+
+// 取り除きたい処理
+axios.interceptors.request.eject(interceptorsRequest);
+axios.interceptors.response.eject(interceptorsResponse);
 
 new Vue({
   router,
